4b89984c144d26f1c0a71ad24e8f7c34c3827ac8
Rojkan <@hrojkan@gmail.com>
2025-11-13 17:29:19 +0000

    version 0.01

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require ('express')
 var ejs = require('ejs')
 const path = require('path')
-
+var mysql = require('mysql2');
 // Create the express application object
 const app = express()
 const port = 8000
@@ -18,6 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Define our application-specific data
 app.locals.shopData = {shopName: "Bertie's Books"}
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'berties_books_app',
+    password: 'qwertyuiop',
+    database: 'berties_books',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
